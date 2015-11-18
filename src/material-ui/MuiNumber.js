538eba46a493c254d44b364a051146dd387a990a
NicholasAzar <nicholas@nicholasazar.com>
2015-11-18 13:02:03 -0500

    Add number input to mui

--- src/material-ui/MuiNumber.js
+++ src/material-ui/MuiNumber.js
@@ -5,35 +5,51 @@ import React from 'react';
 var utils = require('../utils');
 var classNames = require('classnames');
 import ValidationMixin from '../ValidationMixin';
+const TextField = require('material-ui/lib/text-field');
 
+/**
+ * There is no default number picker as part of Material-UI.
+ * Instead, use a TextField and validate.
+ */
 class MuiNumber extends React.Component {
 
-    render() {
-        let formClasses = classNames('form-group', { 'has-error' : this.props.valid === false }, this.props.form.htmlClass, { 'has-success' : this.props.valid === true && this.props.value != null});
-        let labelClasses = classNames('control-label', this.props.form.labelHtmlClass);
-        let fieldClasses = classNames('form-control', this.props.form.fieldHtmlClass);
-
-        let help = this.props.form.description || '';
-        if(!this.props.valid || this.props.form.description) {
-            help = (
-                <div className="help-block">
-                    {this.props.error || this.props.form.description}
-                </div>
-            )
+    constructor(props) {
+        super(props);
+        this.preValidationCheck = this.preValidationCheck.bind(this);
+        this.state = {
+            lastSuccessfulValue : this.props.value
         }
+    }
 
+    isNumeric(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
+
+    /**
+     * Prevent the field from accepting non-numeric characters.
+     * @param e
+     */
+    preValidationCheck(e) {
+        if (this.isNumeric(e.target.value)) {
+            this.setState({
+                lastSuccessfulValue: e.target.value
+            });
+            this.props.onChangeValidate(e);
+        } else {
+            this.refs.numberField.setValue(this.state.lastSuccessfulValue);
+        }
+    }
+
+    render() {
         return (
-            <div className={formClasses}>
-                <label className={labelClasses}>{this.props.form.title}</label>
-                <input type={this.props.form.type}
-                       onChange={this.props.onChangeValidate}
-                       step={this.props.form.step}
-                       className={fieldClasses}
-                       defaultValue={this.props.value}
-                       id={this.props.form.key.slice(-1)[0]}
-                       name={this.props.form.key.slice(-1)[0]}/>
-                {help}
-            </div>
+            <TextField
+                type={this.props.form.type}
+                floatingLabelText={this.props.form.title}
+                hintText={this.props.form.placeholder}
+                errorText={this.props.error}
+                onChange={this.preValidationCheck}
+                defaultValue={this.state.lastSuccessfulValue}
+                ref="numberField"/>
         );
     }
 }
