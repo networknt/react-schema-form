cbdaef8dfeba598e8689e65fd810dc31225862bf
devel-pa <devel.pa@gmail.com>
2016-02-07 17:06:44 +0100

    Minor linting fixes. Make tests work, not passing, yet. Recompiled lib.

--- src/TextArea.js
+++ src/TextArea.js
@@ -23,8 +23,8 @@ class TextArea extends React.Component {
                     rows={this.props.form.rows}
                     rowsMax={this.props.form.rowsMax}
                     disabled={this.props.form.readonly}
-                    style={this.props.form.style || {width: '100%'}}>
-                </TextField>
+                    style={this.props.form.style || {width: '100%'}}
+                />
             </div>
         );
     }
