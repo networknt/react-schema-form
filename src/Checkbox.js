40e4bac5605208ba2c816bfa903608095f824daf
Curtis Wilkinson <curtis@codafication.com>
2017-07-18 10:59:33 +1000

    adding htmlClass to Checkbox.js

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -8,14 +8,16 @@ import Checkbox from 'material-ui/Checkbox';
 class Checkbox2 extends React.Component {
     render() {
         return (
-            <Checkbox
-                name={this.props.form.key.slice(-1)[0]}
-                value={this.props.form.key.slice(-1)[0]}
-                defaultChecked={this.props.value || false}
-                label={this.props.form.title}
-                disabled={this.props.form.readonly}
-                onCheck={(e, checked) => {this.props.onChangeValidate(e)}}
-                />
+            <div className={this.props.form.className}>
+                <Checkbox
+                    name={this.props.form.key.slice(-1)[0]}
+                    value={this.props.form.key.slice(-1)[0]}
+                    defaultChecked={this.props.value || false}
+                    label={this.props.form.title}
+                    disabled={this.props.form.readonly}
+                    onCheck={(e, checked) => {this.props.onChangeValidate(e)}}
+                    />
+             </div>
         );
     }
 }
