19deb2e605d6e57e917f997274c08caeac661e67
Greg Taschuk <gregorytaschuk@gmail.com>
2017-07-28 14:47:03 -0400

    use checked property for checkbox

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -12,7 +12,7 @@ class Checkbox2 extends React.Component {
                 <Checkbox
                     name={this.props.form.key.slice(-1)[0]}
                     value={this.props.form.key.slice(-1)[0]}
-                    defaultChecked={this.props.value || false}
+                    checked={this.props.value || false}
                     label={this.props.form.title}
                     disabled={this.props.form.readonly}
                     onCheck={(e, checked) => {this.props.onChangeValidate(e)}}
