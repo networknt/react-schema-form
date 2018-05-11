313dd571c6643ae65bf254544ccb1096bd91163c
Arjun Nemani <nemaniarjun@gmail.com>
2018-05-12 02:25:55 +0530

    Remove useless props to Date, fix onchange

--- src/Date.js
+++ src/Date.js
@@ -1,25 +1,24 @@
 /**
  * Created by steve on 22/12/15.
  */
-import React from 'react';
-var utils = require('./utils');
-var classNames = require('classnames');
-import ComposedComponent from './ComposedComponent';
-import TextField from 'material-ui/TextField';
+import React from "react";
+var utils = require("./utils");
+var classNames = require("classnames");
+import ComposedComponent from "./ComposedComponent";
+import TextField from "material-ui/TextField";
 
 /**
  * There is no default number picker as part of Material-UI.
  * Instead, use a TextField and validate.
  */
 class Date extends React.Component {
-
     constructor(props) {
         super(props);
         this.onDatePicked = this.onDatePicked.bind(this);
     }
 
-
-    onDatePicked(empty, date) {
+    onDatePicked(e) {
+        let date = e.target.value;
         this.props.onChangeValidate(date);
     }
 
@@ -30,18 +29,17 @@ class Date extends React.Component {
         }
 
         return (
-            <div style={{width: '100%', display: 'block'}} className={this.props.form.htmlClass}>
+            <div
+                style={{ width: "100%", display: "block" }}
+                className={this.props.form.htmlClass}
+            >
                 <TextField
                     id="date"
                     label={this.props.form.title}
                     type="date"
                     onChange={this.onDatePicked}
-                    onShow={null}
-                    onDismiss={null}
-                    value={value}
                     disabled={this.props.form.readonly}
-                    style={this.props.form.style || {width: '100%'}}/>
-
+                />
             </div>
         );
     }
