f13f5121d7566e6ef31276b550b03f197cf705f6
Xavi Torelló <info@xaviertorello.cat>
2018-06-12 20:06:26 +0200

    Fix checkboxes //WIP

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -6,13 +6,32 @@ import ComposedComponent from './ComposedComponent';
 import Checkbox from 'material-ui/Checkbox';
 
 class Checkbox2 extends React.Component {
+
+    constructor (props) {
+        super(props);
+        const {model, form} = this.props;
+        const {key} = form;
+
+        const storedValue = (!_.isNil(this.props.value) && _.isBoolean(this.props.value) )? this.props.value : undefined;
+        const defaultValue = (!_.isNil(form.schema.default) && _.isBoolean(form.schema.default))? form.schema.default : undefined;
+        const value =  storedValue || defaultValue;
+
+        this.props.setDefault(key, model, form, value)
+        this.state = {
+            currentValue: value,
+        };
+    }
+
     render() {
+
+        console.log(this.state.currentValue);
+
         return (
             <div className={this.props.form.className}>
                 <Checkbox
                     name={this.props.form.key.slice(-1)[0]}
                     value={this.props.form.key.slice(-1)[0]}
-                    checked={this.props.value || false}
+                    checked={this.state.currentValue}
                     label={this.props.form.title}
                     disabled={this.props.form.readonly}
                     onCheck={(e, checked) => {this.props.onChangeValidate(e)}}
