b4e31fcfcfb950b293cb0105ba300307f4418017
D.V.D <entropyhacker@gmail.com>
2018-08-07 14:44:38 +0300

    Date: add NativeDateField with common logic for Date and DateTime

--- src/Date.js
+++ src/Date.js
@@ -1,42 +1,7 @@
 /**
  * Created by steve on 22/12/15.
  */
-import React from 'react';
 import ComposedComponent from './ComposedComponent';
-import TextField from '@material-ui/core/TextField';
+import NativeDateField from './NativeDateField'
 
-/**
- * There is no default number picker as part of Material-UI.
- * Instead, use a TextField and validate.
- */
-class DateField extends React.Component {
-    constructor(props) {
-        super(props);
-        this.onDatePicked = this.onDatePicked.bind(this);
-    }
-
-    onDatePicked(e) {
-        let date = new Date(e.target.value);
-        this.props.onChangeValidate(date);
-    }
-
-    render() {
-        let { form, value } = this.props
-        // {shrink: true} fixes rendering of TextField without value
-        // see https://github.com/mui-org/material-ui/issues/8131#issuecomment-328373902
-        return (
-            <TextField
-                id="date"
-                label={form.title}
-                type="date"
-                defaultValue={value}
-                InputLabelProps={{ shrink: true }}
-                onChange={this.onDatePicked}
-                disabled={form.readonly}
-            />
-
-        );
-    }
-}
-
-export default ComposedComponent(DateField);
+export default ComposedComponent(NativeDateField, {type: 'date'});
