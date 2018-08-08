8d4df93b0a231405fe2f84db8fe908aed83042cc
D.V.D <entropyhacker@gmail.com>
2018-08-08 17:11:56 +0300

    FieldSet: converted to MaterialUI 1.0

--- src/FieldSet.js
+++ src/FieldSet.js
@@ -2,24 +2,27 @@
  * Created by steve on 11/09/15.
  */
 import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 class FieldSet extends React.Component {
 
     render() {
         //console.log('FieldSet.render', this.props);
+        let {form, mapper, builder, model, onChange} = this.props
         // now render all the items in the fieldset
-        let forms = this.props.form.items.map(function(form, index){
-            return this.props.builder(form, this.props.model, index, this.props.onChange, this.props.mapper, this.props.builder);
-        }.bind(this));
+        let forms = form.items.map(
+            (f, index) => builder(f, model, index, onChange, mapper, builder)
+        );
 
         return (
-            <fieldset>
-                <legend>{this.props.form.title}</legend>
+            <FormControl component="fieldset">
+                <FormLabel component="legend">{form.title}</FormLabel>
                 <div>
                     {forms}
                 </div>
-            </fieldset>
-        );
+            </FormControl>
+        )
     }
 }
 
