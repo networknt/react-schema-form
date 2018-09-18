44a9e3364bbe658402932769c8657a3fb51584e6
Neil McLaren <neil.mclaren@consensys.net>
2018-09-18 13:27:44 +0100

    Remove extraneous console.log (debugging?) statements.

--- src/Text.js
+++ src/Text.js
@@ -13,8 +13,6 @@ class Text extends React.Component {
         const {model, form, value} = this.props;
         const {key} = form;
 
-        console.log('KKKEEEYYY', key, this.props);
-
         this.props.setDefault(key, model, form, value)
     }
 
