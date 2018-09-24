cbd62dc8ffb44bf6593c8c37e0ba78a243e1308a
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2018-09-24 13:07:30 -0400

    fixes demo

--- webpack.config.js
+++ webpack.config.js
@@ -26,11 +26,6 @@ module.exports = {
       {test: /\.js$/, exclude: /node_modules/, use: [{
         loader: 'babel-loader'
       }]},
-      {test: /\.css?$/, exclude: /node_modules/, use: [{
-        loader: 'style-loader'
-      }, {
-        loader: 'css-loader'
-      }]},
     ]
   }
 };
