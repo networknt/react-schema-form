223afad0b4d0ce7b8acbbe99676715a9366fbd8d
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2018-09-24 15:40:28 -0400

    ensures the select value is never null

--- src/Select.js
+++ src/Select.js
@@ -1,66 +1,62 @@
-/**
- * Created by steve on 15/09/15.
- */
-import React from 'react';
-import ComposedComponent from './ComposedComponent';
-import MenuItem from '@material-ui/core/MenuItem';
-import MuiSelect from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
+import React, {Component} from 'react'
+import ComposedComponent from './ComposedComponent'
+import MenuItem from '@material-ui/core/MenuItem'
+import MuiSelect from '@material-ui/core/Select'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormControl from '@material-ui/core/FormControl'
 
-class Select extends React.Component {
 
-    state = {
-        currentValue: this.getInitialValue(this.props.model, this.props.form)
+const getInitialValue = (model, {key, titleMap}) => {
+    let result
+    if (Array.isArray(key)) {
+        key.reduce((cur, nxt) => cur && cur[nxt], model)
+        result = key.reduce((cur, nxt) => cur && cur[nxt], model)
+    } else {
+        result = model[key]
     }
+    return result || (titleMap != null ? titleMap[0].value : '')
+}
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.model && nextProps.form.key) {
-            this.setState({
-                currentValue: this.getInitialValue(nextProps.model, nextProps.form)
-            });
+class Select extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentValue: getInitialValue(this.props.model, this.props.form)
         }
     }
 
-    getInitialValue(model, form) {
-        return this.getModelValue(model, form.key) || (form.titleMap != null ? form.titleMap[0].value : '')
-    }
-
-    getModelValue(model, key) {
-        if (Array.isArray(key)) {
-            return key.reduce((cur, nxt) => cur && cur[nxt], model);
-        } else {
-            return model[key];
+    static getDerivedStateFromProps(props) {
+        if (props.model && props.form.key) {
+            return {
+                currentValue: getInitialValue(props.model, props.form)
+            }
         }
     }
 
     onSelected = (event) => {
         let currentValue = event.target.value
-        this.setState({
-            currentValue
-        });
-        this.props.onChangeValidate(event);
+        this.setState({currentValue})
+        this.props.onChangeValidate(event)
     }
 
     render() {
-        const { form } = this.props
+        const {form} = this.props
         const menuItems = form.titleMap.map((item, idx) => (
             <MenuItem key={idx} value={item.value}>{item.name}</MenuItem>
-        ));
+        ))
         return (
             <FormControl fullWidth>
                 <InputLabel>{form.title}</InputLabel>
                 <MuiSelect
-                    value={this.state.currentValue}
+                    value={this.state.currentValue || ''}
                     placeholder={form.title}
                     disabled={form.readonly}
-                    onChange={this.onSelected}
-                >
+                    onChange={this.onSelected}>
                     {menuItems}
                 </MuiSelect>
             </FormControl>
-        );
+        )
     }
 }
 
-export default ComposedComponent(Select);
+export default ComposedComponent(Select)
