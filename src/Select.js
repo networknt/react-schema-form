ac817165e9b66a95b36abc10efe6ed6bf23df94d
D.V.D <entropyhacker@gmail.com>
2018-09-26 12:44:08 +0300

    Select: don't use titleMap[0] as initial value for field; get initial value from model only

--- src/Select.js
+++ src/Select.js
@@ -4,31 +4,20 @@ import MenuItem from '@material-ui/core/MenuItem'
 import MuiSelect from '@material-ui/core/Select'
 import InputLabel from '@material-ui/core/InputLabel'
 import FormControl from '@material-ui/core/FormControl'
-
-
-const getInitialValue = (model, {key, titleMap}) => {
-    let result
-    if (Array.isArray(key)) {
-        key.reduce((cur, nxt) => cur && cur[nxt], model)
-        result = key.reduce((cur, nxt) => cur && cur[nxt], model)
-    } else {
-        result = model[key]
-    }
-    return result || (titleMap != null ? titleMap[0].value : '')
-}
+import { getValueFromModel} from './utils'
 
 class Select extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            currentValue: getInitialValue(this.props.model, this.props.form)
+            currentValue: getValueFromModel(this.props.model, this.props.form.key) || ''
         }
     }
 
     static getDerivedStateFromProps(props) {
         if (props.model && props.form.key) {
             return {
-                currentValue: getInitialValue(props.model, props.form)
+                currentValue: getValueFromModel(props.model, props.form.key)
             }
         }
     }
