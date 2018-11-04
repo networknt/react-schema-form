becc14ef3b4eb6c93659ea3761e680e3a140b0b8
Naeem Baghi <naeem@darkpos.com>
2018-11-04 18:44:31 +0330

    SchemaForm: fix eslint errors

--- src/SchemaForm.js
+++ src/SchemaForm.js
@@ -1,41 +1,53 @@
+// @flow
 /**
  * Created by steve on 11/09/15.
  */
-import React from 'react';
-import utils from './utils';
-import Number from './Number';
-import Text from './Text';
-import TextArea from './TextArea';
-import TextSuggest from './TextSuggest';
-import Select from './Select';
-import MultiSelect from './MultiSelect'
-import Radios from './Radios';
-import Date from './Date';
-import Checkbox from './Checkbox';
-import Help from './Help';
-import Array from './Array';
-import FieldSet from './FieldSet';
-import TripleBoolean from './TripleBoolean';
-import merge from 'lodash/merge';
-import isNil from 'lodash/isNil'
+import React, { Component } from "react";
+import merge from "lodash/merge";
+import isNil from "lodash/isNil";
+import utils from "./utils";
+import Number from "./Number";
+import Text from "./Text";
+import TextArea from "./TextArea";
+import TextSuggest from "./TextSuggest";
+import Select from "./Select";
+import MultiSelect from "./MultiSelect";
+import Radios from "./Radios";
+import Date from "./Date";
+import Checkbox from "./Checkbox";
+import Help from "./Help";
+import Array from "./Array";
+import FieldSet from "./FieldSet";
+import TripleBoolean from "./TripleBoolean";
 
-class SchemaForm extends React.Component {
+type Props = {
+    onModelChange: any,
+    errors: any,
+    schema: any,
+    form: any,
+    ignore: any,
+    option: any,
+    model: any,
+    className: any,
+    mapper: any
+};
 
+class SchemaForm extends Component<Props> {
     mapper = {
-        'number': Number,
-        'text': Text,
-        'password': Text,
-        'textarea': TextArea,
-        'textsuggest': TextSuggest,
-        'select': Select,
-        'radios': Radios,
-        'date': Date,
-        'checkbox': Checkbox,
-        'help': Help,
-        'array': Array,
-        'tBoolean': TripleBoolean,
-        'fieldset': FieldSet,
-        'multiselect': MultiSelect
+        number: Number,
+        text: Text,
+        password: Text,
+        textarea: TextArea,
+        textsuggest: TextSuggest,
+        select: Select,
+        radios: Radios,
+        date: Date,
+        checkbox: Checkbox,
+        help: Help,
+        array: Array,
+        tBoolean: TripleBoolean,
+        fieldset: FieldSet,
+        multiselect: MultiSelect
     };
 
     constructor(props) {
@@ -45,67 +57,83 @@ class SchemaForm extends React.Component {
         this.builder = this.builder.bind(this);
     }
 
+    onChange(key, val) {
+        const { onModelChange } = this.props;
+        onModelChange(key, val);
+    }
+
     // Assign default values and save it to the model
     setDefault = (key, model, form, value) => {
-         const currentValue = utils.selectOrSet(key, model);
+        const { onModelChange } = this.props;
+        const currentValue = utils.selectOrSet(key, model);
 
         // If current value is not setted and exist a default, apply the default over the model
         if (isNil(currentValue) && !isNil(value))
-            this.props.onModelChange(key, value, form.type, form);
-    }
-
-    onChange(key, val) {
-        //console.log('SchemaForm.onChange', key, val);
-        this.props.onModelChange(key, val);
-    }
+            onModelChange(key, value, form.type, form);
+    };
 
     builder(form, model, index, mapper, onChange, builder) {
+        const { errors } = this.props;
         const Field = this.mapper[form.type];
-        if(!Field) {
-            // console.log('Invalid field: "' + form.key[0] + '"!');
+        if (!Field) {
             return null;
         }
 
         // Apply conditionals to review if this field must be rendered
-        if(form.condition && utils.safeEval(form.condition, {model}) === false) {
+        if (
+            form.condition &&
+            utils.safeEval(form.condition, { model }) === false
+        ) {
             return null;
         }
 
-        const key = form.key && form.key.join('.') || index;
+        const key = (form.key && form.key.join(".")) || index;
 
-        const errors = this.props && this.props.errors ? this.props.errors : {};
-        let error = (key in errors)? errors[key] : null;
+        const error = key in errors ? errors[key] : null;
 
-        return (<Field
-                    model={model}
-                    form={form}
-                    key={key}
-                    onChange={onChange}
-                    setDefault={this.setDefault}
-                    mapper={mapper}
-                    builder={builder}
-                    errorText={error}
-                />)
+        return (
+            <Field
+                model={model}
+                form={form}
+                key={key}
+                onChange={onChange}
+                setDefault={this.setDefault}
+                mapper={mapper}
+                builder={builder}
+                errorText={error}
+            />
+        );
     }
 
     render() {
-        let merged = utils.merge(this.props.schema, this.props.form, this.props.ignore, this.props.option);
+        const {
+            schema,
+            form,
+            ignore,
+            option,
+            model,
+            className,
+            mapper
+        } = this.props;
+        const merged = utils.merge(schema, form, ignore, option);
 
-        //console.log('SchemaForm merged = ', JSON.stringify(merged, undefined, 2));
-        let mapper = this.mapper;
-        if(this.props.mapper) {
-            mapper = merge(this.mapper, this.props.mapper);
+        let mergedMapper = this.mapper;
+        if (mapper) {
+            mergedMapper = merge(this.mapper, mapper);
         }
-        let forms = merged.map(function(form, index) {
-            return this.builder(form, this.props.model, index, mapper, this.onChange, this.builder);
-        }.bind(this));
-
-        return (
-            <div className={this.props.className}>
-                {forms}
-            </div>
+        const forms = merged.map((formPart, index) =>
+            this.builder(
+                formPart,
+                model,
+                index,
+                mergedMapper,
+                this.onChange,
+                this.builder
+            )
         );
+
+        return <div className={className}>{forms}</div>;
     }
 }
 
-module.exports = SchemaForm;
+export default SchemaForm;
