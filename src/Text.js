f8f762bb41d572a472907a89bb5c7606b814913a
Naeem Baghi <naeem@darkpos.com>
2018-11-15 23:12:33 +0330

    Text: fix eslint errors

--- src/Text.js
+++ src/Text.js
@@ -1,27 +1,36 @@
+// @flow
 /**
  * Created by steve on 15/09/15.
  */
-import React from 'react';
-import ComposedComponent from './ComposedComponent';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import ComposedComponent from "./ComposedComponent";
 
-class Text extends React.Component {
+type Props = {
+    form: any,
+    model: any,
+    value: any,
+    setDefault: any,
+    error: any,
+    onChangeValidate: any
+};
 
+class Text extends React.Component<Props> {
     constructor(props) {
         super(props);
-        const {model, form, value} = this.props;
-        const {key} = form;
-        this.props.setDefault(key, model, form, value)
+        const { model, form, value, setDefault } = this.props;
+        const { key } = form;
+        setDefault(key, model, form, value);
     }
 
     render() {
-        let { form, error, value, onChangeValidate } = this.props
+        const { form, error, value, onChangeValidate } = this.props;
         return (
             <TextField
                 type={form.type}
                 label={form.title}
                 placeholder={form.placeholder}
-                helperText={error || form.description }
+                helperText={error || form.description}
                 error={!!error}
                 onChange={onChangeValidate}
                 value={value}
