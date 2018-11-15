a6c7419e21b40a92fc55a4c3b198577d0e1d7b5c
Naeem Baghi <naeem@darkpos.com>
2018-11-15 23:10:17 +0330

    TextArea: fix eslint errors

--- src/TextArea.js
+++ src/TextArea.js
@@ -1,32 +1,33 @@
+// @flow
 /**
  * Created by steve on 15/09/15.
  */
-import React from 'react';
-import ComposedComponent from './ComposedComponent';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import ComposedComponent from "./ComposedComponent";
 
-class TextArea extends React.Component {
+type Props = {
+    form: any,
+    value: any,
+    error: any,
+    onChangeValidate: any
+};
 
-    render() {
-        let { form, value, error, onChangeValidate } = this.props
-        // FIXME: Obviously fix rowsMax eventually..
-        return (
-            <TextField
-                type={form.type}
-                label={form.title}
-                placeholder={form.placeholder}
-                helperText={error || form.description}
-                onChange={onChangeValidate}
-                error={!!error}
-                value={value}
-                multiline
-                rows={form.rows }
-                rowsMax={form.rowsMax }
-                disabled={form.readonly}
-                fullWidth
-            />
-        )
-    }
-}
+const TextArea = ({ form, value, error, onChangeValidate }: Props) => (
+    <TextField
+        type={form.type}
+        label={form.title}
+        placeholder={form.placeholder}
+        helperText={error || form.description}
+        onChange={onChangeValidate}
+        error={!!error}
+        value={value}
+        multiline
+        rows={form.rows}
+        rowsMax={form.rowsMax}
+        disabled={form.readonly}
+        fullWidth
+    />
+);
 
 export default ComposedComponent(TextArea);
