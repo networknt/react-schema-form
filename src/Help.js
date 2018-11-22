bc4b13df700c39fd0e72f7a8b21ddfebbf5fc44f
Naeem Baghi <naeem@darkpos.com>
2018-11-22 17:59:24 +0330

    Help: fix eslint errors

--- src/Help.js
+++ src/Help.js
@@ -1,16 +1,16 @@
+// @flow
 /**
  * Created by steve on 20/09/15.
  */
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
 
-class Help extends React.Component{
-    render() {
-        let {description} = this.props.form
-        return (
-            <Typography variant="body2">{description}</Typography>
-        )
-    }
-}
+type Props = {
+    form: any
+};
+
+const Help = ({ form: { description } }: Props) => (
+    <Typography variant="body2">{description}</Typography>
+);
 
 export default Help;
