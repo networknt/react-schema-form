6cd99e83448e1e57acf27efe6bde672a6e3d1109
Naeem Baghi <naeem@darkpos.com>
2018-11-25 18:25:38 +0330

    TextSuggest: simple eslint fixes

--- src/TextSuggest.js
+++ src/TextSuggest.js
@@ -3,53 +3,51 @@
 /**
  * Created by XaviTorello on 30/05/18
  */
-import React from 'react';
-import ComposedComponent from './ComposedComponent';
+import React from "react";
+import ComposedComponent from "./ComposedComponent";
 
 const dataSourceConfig = {
-  text: 'name',
-  value: 'value',
+    text: "name",
+    value: "value"
 };
 
 class TextSuggest extends React.Component {
     handleUpdate = (newValue, index) => {
-      const {key} = this.props.form
-      const {type} = this.props.form.schema
-      return this.props.onChange(key, newValue[dataSourceConfig['value']], type, this.props.form)
+        const { key } = this.props.form;
+        const { type } = this.props.form.schema;
+        return this.props.onChange(
+            key,
+            newValue[dataSourceConfig.value],
+            type,
+            this.props.form
+        );
     };
 
-    handleInit = (init_value) => {
+    handleInit = init_value => {
         if (!this.props.form.schema || !this.props.form.schema.enum)
-            return init_value.toString()
+            return init_value.toString();
 
-        const names = this.props.form.schema.enumNames || this.props.form.schema.enum;
+        const names =
+            this.props.form.schema.enumNames || this.props.form.schema.enum;
         const values = this.props.form.schema.enum;
 
-        // console.log(names, values);
-        // console.log("indexOf", values.indexOf(init_value));
-        // console.log("names[values.indexOf(init_value)]", names[values.indexOf(init_value)]);
         const init_value_name = names[values.indexOf(init_value)];
 
         // this.handleUpdate({[dataSourceConfig['value']]: init_value, [dataSourceConfig['text']]: init_value_name})
 
-        return init_value_name || init_value.toString()
-    }
+        return init_value_name || init_value.toString();
+    };
 
     render() {
-        // console.log('TextSuggest', this.props);
         // assign the filter, by default case insensitive
-        const filter = ((filter) => {
+        const filter = (filter => {
             switch (filter) {
-                case 'fuzzy':
+                case "fuzzy":
                     return AutoComplete.fuzzyFilter;
-                    break;
                 default:
                     return AutoComplete.caseInsensitiveFilter;
-                    break;
             }
-        })(this.props.form.filter)
-
-        // console.log("TEXTSUG", this.props);
+        })(this.props.form.filter);
 
         const value = this.props.value && this.handleInit(this.props.value);
 
@@ -62,10 +60,10 @@ class TextSuggest extends React.Component {
                     errorText={this.props.error}
                     onNewRequest={this.handleUpdate}
                     disabled={this.props.form.readonly}
-                    style={this.props.form.style || {width: '100%'}}
+                    style={this.props.form.style || { width: "100%" }}
                     openOnFocus
                     searchText={value}
-                    dataSource={this.props.form.titleMap || ['Loading...']}
+                    dataSource={this.props.form.titleMap || ["Loading..."]}
                     filter={filter}
                     maxSearchResults={this.props.form.maxSearchResults || 5}
                     dataSourceConfig={dataSourceConfig}
