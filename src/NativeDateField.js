adf7df077fb0dc8c2732ca2d9b4fc54aa0f2cf65
Naeem Baghi <naeem@darkpos.com>
2018-11-25 17:28:59 +0330

    NativeDateField: bug fix for model

--- src/NativeDateField.js
+++ src/NativeDateField.js
@@ -13,20 +13,25 @@ type Props = {
     type: any
 };
 
-const NativeDateField = ({ form, value, type, onChangeValidate }: Props) => (
-    <TextField
-        label={form.title}
-        type={type}
-        value={
-            (value &&
-                typeof value === "object" &&
-                value.toISOString().slice(0, 10)) ||
-            ""
-        }
-        InputLabelProps={{ shrink: true }}
-        onChange={onChangeValidate}
-        disabled={form.readonly}
-    />
-);
+const NativeDateField = ({ form, value, type, onChangeValidate }: Props) => {
+    let fieldValue =
+        (value &&
+            typeof value === "object" &&
+            value.toISOString().slice(0, 10)) ||
+        value;
+    if (!fieldValue) fieldValue = "";
+    if (fieldValue.length > 0)
+        fieldValue = new Date(fieldValue).toISOString().slice(0, 10);
+    return (
+        <TextField
+            label={form.title}
+            type={type}
+            value={fieldValue}
+            InputLabelProps={{ shrink: true }}
+            onChange={onChangeValidate}
+            disabled={form.readonly}
+        />
+    );
+};
 
 export default NativeDateField;
